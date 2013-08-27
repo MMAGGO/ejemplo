9ee379f83d05a24f55597ddac9e218ab10d66df1
Jonathan López <jlopez@fipasoft.com.mx>
2013-08-27 17:56:21 -0500

    Función suma.

--- Ejemplo/Ejemplo/Main.cs
+++ Ejemplo/Ejemplo/Main.cs
@@ -4,6 +4,13 @@ namespace Ejemplo
 {
 	class MainClass
 	{
+
+		public int suma(){
+			int a = 10;
+			int b = 30;
+			return a+b;
+		}
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
